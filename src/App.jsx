e14ad1b85d70e4955d330af90dfe5f7758b5c37a
ebi2kh <111044086+ebi2kh@users.noreply.github.com>
2024-05-02 16:24:47 +0330

    inital commet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => (
+  <>
+    {/* Mirrored from istgahedandan.ir/rahmani/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 08 Dec 2023 17:59:56 GMT */}
+
+    <div>hello</div>
+  </>
+);
+
+export default App;
